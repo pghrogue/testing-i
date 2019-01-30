52ceaad4268beeb6e72144e0fc0b66a84c90d340
Jennifer King <jenbellpgh@gmail.com>
2019-01-29 23:26:47 -0500

    Started setup. Created shell functions in modItems.js

---
+++ modItems.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  // Success(item) method accepts an item object and returns a new item object modified
+  // according to the rules defined.
+  success: (item) => {},
+
+  // Fail(item) method accepts an item object and returns a new item object modified 
+  // according to the rules.
+  fail: (item) => {},
+
+  // Repair(item) method accepts an item object and returns a new item object with
+  // durability restored to 100.
+  repair: (item) => {}
+};
