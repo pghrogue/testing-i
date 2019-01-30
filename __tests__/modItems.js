e4f7055bd4c850561cc2ad55fce4392226621b76
Jennifer King <jenbellpgh@gmail.com>
2019-01-29 23:44:13 -0500

    Some more skeleton in place. Trying to think of how to do this.

---
+++ __tests__/modItems.js
@@ -0,0 +1,25 @@
+const modItem = require('../modItems');
+
+// Some default items to test with:
+const weapon = {};
+const armor = {};
+
+// Test weapon specific:
+describe('weapon enhancements:', () => {
+  test('success', () => {});
+
+  test('fail', () => {});
+
+  test('repair', () => {
+    
+  });
+});
+
+// Test armor specific:
+describe('armor enhancements', () => {
+  test('success', () => {});
+
+  test('fail', () => {});
+
+  test('repair', () => {});
+});
